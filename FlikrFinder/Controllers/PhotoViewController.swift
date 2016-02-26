089802495d3d21d1d2b2ec8610b4b6bfc1dcd05e
Andrey Torlopov <torlopov.andrey@gmail.com>
2016-02-26 22:44:49 +0300

    fix scrollview

--- FlikrFinder/Controllers/PhotoViewController.swift
+++ FlikrFinder/Controllers/PhotoViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 
 //MARK:- lifecycle
-class PhotoViewController: UIViewController
-{
+class PhotoViewController: UIViewController {
     //MARK: properties
     var photo: Photo?
+    var defaultInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     
-
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
         assert(photo != nil)
         setupViews()
         savePhotoInHistory()
+        self.view.backgroundColor =  UIColor.flatOrangeColor()
     }
 }
 
@@ -63,11 +62,52 @@ extension PhotoViewController
 }
 
 //MARK:- scrollview delegate
-extension PhotoViewController: UIScrollViewDelegate
-{
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView?
-    {
+extension PhotoViewController: UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return imageView
+    }
+}
+
+
+//MARK:- scroll view correct place
+extension PhotoViewController {
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        
+        let contentWidth = CGRectGetWidth(imageView.frame)
+        let scrollWidth  = CGRectGetWidth(scrollView.frame)
+        var horizontalMargin: CGFloat = 0
+        
+        if contentWidth + defaultInset.left + defaultInset.right < scrollWidth {
+            horizontalMargin = (scrollWidth - contentWidth - (defaultInset.left + defaultInset.right )) / CGFloat(2)
+            //            print("scrollWidth = \(scrollWidth) contentWidth = \(contentWidth) horizontalMargin = \(horizontalMargin)")
+        }
+        
+        var inset = scrollView.contentInset
+        
+        inset.left  = horizontalMargin
+        inset.right = horizontalMargin
+        
+        let contentHeight = CGRectGetHeight(imageView.frame)
+        let scrollHeight = CGRectGetHeight(scrollView.frame)
+        
+        var verticalMargin: CGFloat = 0
+        
+        if contentHeight + defaultInset.top + defaultInset.bottom < scrollHeight {
+            verticalMargin = (scrollHeight - contentHeight - (defaultInset.top + defaultInset.bottom)) / CGFloat(2)
+        }
+        
+        inset.top = verticalMargin
+        inset.bottom = verticalMargin
+        
+        if (scrollView.contentInset == inset){
+            return
+        }
+        
+        UIView.animateWithDuration(0.5) { () -> Void in
+            scrollView.contentInset = inset
+            scrollView.layoutIfNeeded()
+        }
+        
     }
 }
 
